815e0f76ce14810157cf83659a2f9d1098d94551
gdcp <gdcp@m19.local>
2018-03-07 15:50:54 +0800

    1\hfhfhhfhhfhfh

--- first/ViewController.swift
+++ first/ViewController.swift
@@ -11,8 +11,11 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        print("hhfhfhhgh")
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
